87f79166f52245a9ec14854e9bea3e59213aad47
darpan <darpanshah7@gmail.com>
2026-02-11 17:19:32 -0500

    initial commit

---
+++ src/main/java/com/gdit/technicalassessment/GditTechnicalAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.gdit.technicalassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GditTechnicalAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GditTechnicalAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdit/technicalassessment/GditTechnicalAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdit.technicalassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GditTechnicalAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
